e37307cdc1e03d2c22d85f032d587bac3a3c6011
исп-41 <-41@LAB44-01>
2022-10-10 15:33:43 +0300

    Добавьте файлы проекта.

---
+++ app4/app4/Page1.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (check.IsChecked == true)
+            {
+                group1.IsEnabled = true;
+            }
+            else
+            {
+                group1.IsEnabled = false;
+            }
+        }
+
+        private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+            switch(radioButton.Content)
+            {
+                case "Австрия":
+                    country.Text = "Столица: Вена";
+                    break;
+                case "Болгария":
+                    country.Text = "Столица: София";
+                    break;
+                case "Греция":
+                    country.Text = "Столица: Афины";
+                    break;
+                case "Италия":
+                    country.Text = "Столица: Венеция";
+                    break;
+                case "Норвегия":
+                    country.Text = "Столица: Осло";
+                    break;
+                case "Польша":
+                    country.Text = "Столица: Варшава";
+                    break;
+                case "Франция":
+                    country.Text = "Столица: Париж";
+                    break;
+                case "Испания":
+                    country.Text = "Столица: Мадрид";
+                    break;
+            }
+        }
+    }
+}
---
+++ app4/app4/Page2.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace app4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (switchToggle.IsToggled == true)
+            {
+                group1.IsEnabled = true;
+                countryButton.IsEnabled = true;
+            }
+            else
+            {
+                group1.IsEnabled = false;
+                countryButton.IsEnabled = false;
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            RadioButton radioButton = default;
+            foreach (var item in group1.Children)
+            {
+                if ((item as RadioButton).IsChecked == true)
+                {
+                    radioButton = item as RadioButton;
+                    break;
+                }
+            }
+            if (radioButton == null) return;
+            switch (radioButton.Content)
+            {
+                case "Австрия":
+                    country.Text = "Столица: Вена";
+                    break;
+                case "Болгария":
+                    country.Text = "Столица: София";
+                    break;
+                case "Греция":
+                    country.Text = "Столица: Афины";
+                    break;
+                case "Италия":
+                    country.Text = "Столица: Венеция";
+                    break;
+                case "Норвегия":
+                    country.Text = "Столица: Осло";
+                    break;
+                case "Польша":
+                    country.Text = "Столица: Варшава";
+                    break;
+                case "Франция":
+                    country.Text = "Столица: Париж";
+                    break;
+                case "Испания":
+                    country.Text = "Столица: Мадрид";
+                    break;
+            }
+        }
+
+        private void RadioButton_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            country.Text = "Столица:";
+        }
+    }
+}
